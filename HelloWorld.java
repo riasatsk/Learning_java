d59dbd11646a4236e34f574616304b3638ccece1
Riasat Sk <riasat.sk@outlook.com>
2025-06-03 19:39:36 +0530

    Changes to be committed: 	new file:   HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Hello("Riasat");
+    }
+    public static void Hello(String name){
+        System.out.println("Hello World My name is " + name);
+    }
+}
